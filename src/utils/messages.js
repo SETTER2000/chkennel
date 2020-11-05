924ebfb321d3edcd9d33f20b5931f1ac2af4ff4e
Александр Александр <lphp@mail.ru>
2020-11-05 17:58:43 +0300

    App init

---
+++ src/utils/messages.js
@@ -0,0 +1,7 @@
+export default {
+    'logout': 'Logout',
+    'login':'Для начала войдите с систему',
+    'auth/user-not-found':'Пользователя с таким email не существует',
+    'auth/wrong-password':'Неверный пароль',
+    'auth/email-already-in-use':'Email уже занят',
+}
